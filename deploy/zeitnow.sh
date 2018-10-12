a9bdd0ddc7c631dc6b66170989280687bc9b19e7
Julien Rialland <julien.rialland@gmail.com>
2018-10-12 19:49:33 +0200

    smaller docker image

--- deploy/zeitnow.sh
+++ deploy/zeitnow.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+
+set -euo pipefail
+
 thisscript=$(readlink -m $0)
 thisdir=$(dirname $thisscript)
 basedir=$(readlink -m $thisdir/..)
 
-mv $basedir/target/*.war ./
+
+warfile=$(find $basedir/target -type f -name "*.war" |head -1)
+
+if [ "$warfile" == "" ]; then
+    pushd $basedir
+    mvn clean package
+    popd
+    warfile=$(find $basedir/target -type f -name "*.war" |head -1)
+fi 
+
+exploded=$(echo $warfile|sed s/.war//)
+echo 'copying ' $exploded
+rm ./ROOT -rf
+cp $exploded ./ROOT -R
 
 now --public --token $NOW_TOKEN && now alias --token $NOW_TOKEN
 
